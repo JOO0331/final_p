2ec18119875455f03f89b1ffb0e86f56c704a943
thingroot <thingroot97@gmail.com>
2024-12-03 00:50:13 +0900

    Revert "검색, 대시보드 임시 연동"
    This reverts commit c8e6ca32a74b5aa04f677b36148623df631602ce.

--- final_ppProject수정/functions/views.py
+++ final_ppProject수정/functions/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from .models import Game
-import re
 
 def main_view(request):
     games = Game.objects.all()
@@ -10,38 +9,11 @@ def main_view(request):
     return render(request, "main_gpt.html", {'games': games})
 
 def search_view(request):
-    query = request.GET.get('search', '')
     games = Game.objects.all()
-    
-    if query:
-        games = games.filter(name__icontains=query)
-    
-    for game in games:
-        game.final_price_int = int(float(str(game.final_price)))
-        game.initial_price_int = int(float(str(game.initial_price)))
-    
     return render(request, "search_gpt.html", {'games': games})
 
-def dashboard_view(request, app_id):
-    game = Game.objects.get(app_id=app_id)
-    game.final_price_int = int(float(str(game.final_price)))
-    game.initial_price_int = int(float(str(game.initial_price)))
-    
-    if isinstance(game.tags, str):
-        game.tags = [tag.strip() for tag in game.tags.split(',')]
-    
-    if isinstance(game.supported_languages, str):
-        clean_text = re.sub('<[^<]+?>', '', game.supported_languages)
-        main_languages = clean_text.split('languages with full audio support')[0]
-        languages = [lang.strip() for lang in main_languages.split(',')]
-        game.supported_languages = [lang.replace('*', '').strip() for lang in languages]
-    
-    if isinstance(game.developers, str):
-        game.developers = [dev.strip() for dev in game.developers.split(',')]
-    
-    if isinstance(game.publishers, str):
-        game.publishers = [pub.strip() for pub in game.publishers.split(',')]
-    
+def dashboard_view(request):
+    game = Game.objects.get(app_id=105600)
     return render(request, 'dash_gpt.html', {'game': game})
 
 
